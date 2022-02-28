35ed165fdf769edca29350713c0feda07226b6d2
0bserver <rlee767@gmail.com>
2022-03-01 00:32:35 +0800

    Game board is working, can place player 1 and 2 tokens, next to work on showing image when hovering over the selector row-0; and checking score table

--- Project 1/connect4.js
+++ Project 1/connect4.js
@@ -6,14 +6,16 @@
 // const player2 = "p2"; // yellow
 
 const actionIn = (chicken) => {
-  const selectedEmpty = chicken.currentTarget.innerText;
-  // console.log(selectedEmpty);
-  const allCells = document.querySelectorAll(".empty");
-  // console.log(allCells);
+  const selectedEmpty = chicken.currentTarget.innerText; //checks the inner text
+  // console.log(selectedEmpty); // console.log the inner text
+  const allCells = document.querySelectorAll(".empty"); // selecting all with class .empty and displaying as an array
+  // console.log(allCells); // displaying const allCells
   for (let i = allCells.length - 1; i > 0; i--) {
+    // checking the cells from bottom up
+    // console.log(i);
     if (
-      allCells[i].innerText[1] == selectedEmpty &&
-      document.querySelector("#currentPlayer").innerText == "Player 1"
+      allCells[i].innerText[1] == selectedEmpty && // checking the cells from bottom up to see if they match selectedEmpty
+      document.querySelector("#currentPlayer").innerText == "Player 1" // additional requirement to check which player it is and assign color related to player
     ) {
       allCells[i].style.backgroundColor = "red";
 
@@ -34,13 +36,15 @@ const actionOut = () => {
     leftEmpty[i].style.backgroundColor = "aquamarine";
   }
 };
-// upon click, i need to change bkgd-clr to player color and remove class empty/ add class filled ---------- this is where i left off
-const clicked = (e) => {
-  const fillCell = e.target.actionIn;
-  const allCells = document.querySelectorAll(".empty");
-  document.querySelector(".empty").addClass("filled");
-  console.log(allCells);
-};
+
+// const clicked = (e) => {
+//   const fillCell = e.target.classList;
+//   fillCell.addClass("filled");
+// const allCells = document.querySelectorAll(".empty");
+// console.log(allCells);
+// document.querySelector(".empty").addClass("filled");
+// console.log(allCells);
+// };
 
 // if (document.querySelector("#currentPlayer").innerText == "Player 1") {
 //   // console.log("Player 1");
@@ -48,6 +52,8 @@ const clicked = (e) => {
 // } else {
 //   selectedCell.style.backgroundColor = "yellow";
 // }
+
+/////////////// ----------- Switching Player
 
 const changePlayer = (e) => {
   if (document.querySelector("#currentPlayer").innerText == "Player 1") {
@@ -57,23 +63,55 @@ const changePlayer = (e) => {
   }
 };
 
+////////////// ------------ Check Table
+
+const checkTable = (e) => {
+  document.querySelector(".filled");
+};
+
 document.querySelector("#submitBtn").addEventListener("click", changePlayer);
+document.querySelector("#submitBtn").addEventListener("click", checkTable);
 
 // const actionOut = (chicken) => {
 //   const selectedCell = chicken.currentTarget;
 //   selectedCell.style.backgroundColor = "aquamarine";
 // };
 
-const playerChipIn = (e) => {};
+// upon click, i need to change bkgd-clr to player color and remove class empty/ add class filled ---------- this is where i left off
 
-// const colorChange = (chicken) => {
-//     const colorSwitch = chicken.
-// }
+const clicked = (chicken) => {
+  const selectedEmpty = chicken.currentTarget.innerText; //checks the inner text
+  // console.log(selectedEmpty); // console.log the inner text
+  const allCells = document.querySelectorAll(".empty"); // selecting all with class .empty and displaying as an array
+  // console.log(allCells); // displaying const allCells
+  for (let i = allCells.length - 1; i > 0; i--) {
+    // checking the cells from bottom up
+    // console.log(i);
+    if (
+      allCells[i].innerText[1] == selectedEmpty && // checking the cells from bottom up to see if they match selectedEmpty
+      document.querySelector("#currentPlayer").innerText == "Player 1" // additional requirement to check which player it is and assign color related to player
+    ) {
+      allCells[i].style.backgroundColor = "red";
+      allCells[i].classList.add("filled");
+      allCells[i].classList.remove("empty");
+      return allCells[i]; //
+    } else if (
+      allCells[i].innerText[1] == selectedEmpty &&
+      document.querySelector("#currentPlayer").innerText == "Player 2"
+    ) {
+      allCells[i].style.backgroundColor = "yellow";
+      allCells[i].classList.add("filled");
+      allCells[i].classList.remove("empty");
+      // console.log(allCells[i]);
+      return allCells[i];
+    }
+  }
+};
 
 const slotCell = document.querySelectorAll(".row-0");
 // console.log(gameSlot);
-slotCell.forEach((slot) => slot.addEventListener("mouseenter", actionIn));
-slotCell.forEach((slot) => slot.addEventListener("mouseleave", actionOut));
+slotCell.forEach((slot) => slot.addEventListener("mouseenter", actionIn)); //highlight cell when mousing over
+slotCell.forEach((slot) => slot.addEventListener("mouseleave", actionOut)); //remove highlighted cell when leaving cell
 slotCell.forEach((chicken) => chicken.addEventListener("click", clicked));
 // slotCell.addEventListener("click", playerChipIn);
 // slotCell.on("mouseenter", "", colorChange);
