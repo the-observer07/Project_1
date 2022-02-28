a3ee115ddec5593b5bbde3a87ec5541522feb1ff
0bserver <rlee767@gmail.com>
2022-02-28 15:07:24 +0800

    28/2 15:07hrs

--- Project 1/connect4.js
+++ Project 1/connect4.js
@@ -5,25 +5,56 @@
 const player1 = "p1"; // red
 const player2 = "p2"; // yellow
 
-const action = (chicken) => {
-  const selectedCell = chicken.target.classList;
-  console.log(selectedCell);
+const actionIn = (chicken) => {
+  const selectedCell = chicken.currentTarget;
+
+  //
+
+  if (document.querySelector("#currentPlayer").innerText == "Player 1") {
+    // console.log("Player 1");
+    selectedCell.style.backgroundColor = "red";
+  } else {
+    selectedCell.style.backgroundColor = "yellow";
+  }
+};
+
+const changePlayer = (e) => {
+  if (document.querySelector("#currentPlayer").innerText == "Player 1") {
+    document.querySelector("#currentPlayer").innerText = "Player 2";
+  } else {
+    document.querySelector("#currentPlayer").innerText = "Player 1";
+  }
+};
+
+document.querySelector("#submitBtn").addEventListener("click", changePlayer);
+
+const actionOut = (chicken) => {
+  const selectedCell = chicken.currentTarget;
+  selectedCell.style.backgroundColor = "aquamarine";
 };
 
 // const playerChipIn = (chicken) => {
-//     const
-.addClass('addColor')
+//     .addClass('addColor')
 // };
 
 // const colorChange = (chicken) => {
 //     const colorSwitch = chicken.
 // }
 
-const slotCell = document.getElementById("board-int");
+const slotCell = document.querySelectorAll(".row-0");
 // console.log(gameSlot);
-slotCell.addEventListener("mouseover", action);
-slotCell.addEventListener("click", playerChipIn);
-// slotCell.on("mouseenter", ".row-0", colorChange);
+slotCell.forEach((slot) => slot.addEventListener("mouseenter", actionIn));
+slotCell.forEach((slot) => slot.addEventListener("mouseleave", actionOut));
+// slotCell.addEventListener("click", playerChipIn);
+// slotCell.on("mouseenter", "", colorChange);
 // slotCell.on("mouseleave");
 //find empty cell -- check if cell class = empty
 //hover - background color change
+//submit button to change player?
+//on click to change player?
+
+//addClass - filled
+//removeClass - empty // hover unapplied
+
+// if innerText = player1 use button to change to player2
+// toggle class for p1 and player2
