fb9379debe8a57f29d61e6b0101187544511ae41
0bserver <rlee767@gmail.com>
2022-02-28 11:04:43 +0800

    First Upload

---
+++ Project 1/connect4.js
@@ -0,0 +1,9 @@
+// addEventListener(){
+//     const =
+// }
+
+const gameSlot = document.getElementById("board-int");
+// console.log(gameSlot);
+gameSlot.addEventListener("mouseover", function (event) {
+  console.log(event);
+});
