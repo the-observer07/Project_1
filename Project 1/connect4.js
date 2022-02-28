f659eb898e21daffe52d1a8a9ffce3c2e5954e3e
0bserver <rlee767@gmail.com>
2022-02-28 14:13:33 +0800

    newsave

--- Project 1/connect4.js
+++ Project 1/connect4.js
@@ -2,8 +2,28 @@
 //     const =
 // }
 
-const gameSlot = document.getElementById("board-int");
+const player1 = "p1"; // red
+const player2 = "p2"; // yellow
+
+const action = (chicken) => {
+  const selectedCell = chicken.target.classList;
+  console.log(selectedCell);
+};
+
+// const playerChipIn = (chicken) => {
+//     const
+.addClass('addColor')
+// };
+
+// const colorChange = (chicken) => {
+//     const colorSwitch = chicken.
+// }
+
+const slotCell = document.getElementById("board-int");
 // console.log(gameSlot);
-gameSlot.addEventListener("mouseover", function (event) {
-  console.log(event);
-});
+slotCell.addEventListener("mouseover", action);
+slotCell.addEventListener("click", playerChipIn);
+// slotCell.on("mouseenter", ".row-0", colorChange);
+// slotCell.on("mouseleave");
+//find empty cell -- check if cell class = empty
+//hover - background color change
